a8bec082d0c43006c5af615a135407d122976c59
Mingxin Wang <mingxwa@microsoft.com>
2024-10-23 16:29:17 +0800

    test

--- benchmarks/proxy_invocation_benchmark.cpp
+++ benchmarks/proxy_invocation_benchmark.cpp
@@ -9,49 +9,74 @@
 
 namespace {
 
-template <std::invocable F>
-class ContextualFixture : public benchmark::Fixture {
- public:
-  void SetUp(::benchmark::State&) override { Context = F{}(); }
+std::vector<pro::proxy<InvocationTestFacade>> SmallObjectInvocationProxyTestData;
+std::vector<std::unique_ptr<InvocationTestBase>> SmallObjectInvocationVirtualFunctionTestData;
+std::vector<pro::proxy<InvocationTestFacade>> LargeObjectInvocationProxyTestData;
+std::vector<std::unique_ptr<InvocationTestBase>> LargeObjectInvocationVirtualFunctionTestData;
 
-  decltype(F{}()) Context;
-};
-#define CONTEXTUAL_BENCHMARK(___NAME, ___F) BENCHMARK_TEMPLATE_F(ContextualFixture, ___NAME, decltype([] { return ___F(); }))
-
-CONTEXTUAL_BENCHMARK(BM_SmallObjectInvocationViaProxy, GenerateSmallObjectInvocationProxyTestData)(benchmark::State& st) {
-  for (auto _ : st) {
-    for (auto& p : this->Context) {
+void BM_SmallObjectInvocationViaProxy(benchmark::State& state) {
+  for (auto _ : state) {
+    for (auto& p : SmallObjectInvocationProxyTestData) {
       int result = p->Fun();
       benchmark::DoNotOptimize(result);
     }
   }
 }
+void BM_SmallObjectInvocationViaProxy_Setup(const benchmark::State&) {
+  SmallObjectInvocationProxyTestData = GenerateSmallObjectInvocationProxyTestData();
+}
+void BM_SmallObjectInvocationViaProxy_Teardown(const benchmark::State&) {
+  SmallObjectInvocationProxyTestData.clear();
+}
 
-CONTEXTUAL_BENCHMARK(BM_SmallObjectInvocationViaVirtualFunction, GenerateSmallObjectInvocationVirtualFunctionTestData)(benchmark::State& st) {
-  for (auto _ : st) {
-    for (auto& p : this->Context) {
+void BM_SmallObjectInvocationViaVirtualFunction(benchmark::State& state) {
+  for (auto _ : state) {
+    for (auto& p : SmallObjectInvocationVirtualFunctionTestData) {
       int result = p->Fun();
       benchmark::DoNotOptimize(result);
     }
   }
 }
+void BM_SmallObjectInvocationViaVirtualFunction_Setup(const benchmark::State&) {
+  SmallObjectInvocationVirtualFunctionTestData = GenerateSmallObjectInvocationVirtualFunctionTestData();
+}
+void BM_SmallObjectInvocationViaVirtualFunction_Teardown(const benchmark::State&) {
+  SmallObjectInvocationVirtualFunctionTestData.clear();
+}
 
-CONTEXTUAL_BENCHMARK(BM_LargeObjectInvocationViaProxy, GenerateLargeObjectInvocationProxyTestData)(benchmark::State& st) {
-  for (auto _ : st) {
-    for (auto& p : this->Context) {
+void BM_LargeObjectInvocationViaProxy(benchmark::State& state) {
+  for (auto _ : state) {
+    for (auto& p : LargeObjectInvocationProxyTestData) {
       int result = p->Fun();
       benchmark::DoNotOptimize(result);
     }
   }
 }
+void BM_LargeObjectInvocationViaProxy_Setup(const benchmark::State&) {
+  LargeObjectInvocationProxyTestData = GenerateLargeObjectInvocationProxyTestData();
+}
+void BM_LargeObjectInvocationViaProxy_Teardown(const benchmark::State&) {
+  LargeObjectInvocationProxyTestData.clear();
+}
 
-CONTEXTUAL_BENCHMARK(BM_LargeObjectInvocationViaVirtualFunction, GenerateLargeObjectInvocationVirtualFunctionTestData)(benchmark::State& st) {
-  for (auto _ : st) {
-    for (auto& p : this->Context) {
+void BM_LargeObjectInvocationViaVirtualFunction(benchmark::State& state) {
+  for (auto _ : state) {
+    for (auto& p : LargeObjectInvocationVirtualFunctionTestData) {
       int result = p->Fun();
       benchmark::DoNotOptimize(result);
     }
   }
 }
+void BM_LargeObjectInvocationViaVirtualFunction_Setup(const benchmark::State&) {
+  LargeObjectInvocationVirtualFunctionTestData = GenerateLargeObjectInvocationVirtualFunctionTestData();
+}
+void BM_LargeObjectInvocationViaVirtualFunction_Teardown(const benchmark::State&) {
+  LargeObjectInvocationVirtualFunctionTestData.clear();
+}
+
+BENCHMARK(BM_SmallObjectInvocationViaProxy)->Setup(BM_SmallObjectInvocationViaProxy_Setup)->Teardown(BM_SmallObjectInvocationViaProxy_Teardown);
+BENCHMARK(BM_SmallObjectInvocationViaVirtualFunction)->Setup(BM_SmallObjectInvocationViaVirtualFunction_Setup)->Teardown(BM_SmallObjectInvocationViaVirtualFunction_Teardown);
+BENCHMARK(BM_LargeObjectInvocationViaProxy)->Setup(BM_LargeObjectInvocationViaProxy_Setup)->Teardown(BM_LargeObjectInvocationViaProxy_Teardown);
+BENCHMARK(BM_LargeObjectInvocationViaVirtualFunction)->Setup(BM_LargeObjectInvocationViaVirtualFunction_Setup)->Teardown(BM_LargeObjectInvocationViaVirtualFunction_Teardown);
 
 }  // namespace
