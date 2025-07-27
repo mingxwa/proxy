181d8d594a32be6729c4c2ee6d1f681b2ebff4eb
Ubuntu <azureuser@ub.yroqm2j22wpejfacywa2pejuhf.rx.internal.cloudapp.net>
2025-07-27 01:25:24 +0000

    .

--- include/proxy/v4/proxy.h
+++ include/proxy/v4/proxy.h
@@ -2361,8 +2361,8 @@ using rtti = indirect_rtti<FB>;
 #endif // __cpp_rtti >= 199711L
 
 template <class FB>
-using slim = typename FB::template restrict_layout<
-    sizeof(void*), alignof(void*)>;
+using slim =
+    typename FB::template restrict_layout<sizeof(void*), alignof(void*)>;
 
 template <class FB>
 using as_view = typename FB::template add_direct_convention<
