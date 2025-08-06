44e46e0947bb94a46fa41cddb5d3a00aaa6ef254
Mingxin Wang <mingxwa@microsoft.com>
2025-08-06 13:31:40 +0800

    .

--- include/proxy/v4/proxy.h
+++ include/proxy/v4/proxy.h
@@ -1459,7 +1459,7 @@ public:
   }
   auto get_weak() const noexcept {
     return converter{
-        [ptr = this->ptr_]<class F>(std::in_place_type_t<proxy<F>>) noexcept {
+        [ptr = this->ptr_]<class F>(std::in_place_type_t<proxy<F>>) noexcept -> proxy<F> {
           ptr->weak_count.fetch_add(1, std::memory_order::relaxed);
           return proxy<F>{std::in_place_type<weak_compact_ptr<T, Alloc>>, ptr};
         }};
@@ -1495,8 +1495,7 @@ public:
     }
   }
   auto lock() const noexcept {
-    return converter{[ptr = this->ptr_]<class F>(
-                         std::in_place_type_t<proxy<F>>) noexcept {
+    return converter{[ptr = this->ptr_]<class F>(std::in_place_type_t<proxy<F>>) noexcept -> proxy<F> {
       long ref_count = ptr->strong_count.load(std::memory_order::relaxed);
       do {
         if (ref_count == 0) {
