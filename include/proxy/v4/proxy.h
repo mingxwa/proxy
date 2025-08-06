04c3a38acb2245ad014a11d99dd04d22ae7d8dff
Ubuntu <azureuser@ub.yroqm2j22wpejfacywa2pejuhf.rx.internal.cloudapp.net>
2025-08-06 05:07:01 +0000

    .

--- include/proxy/v4/proxy.h
+++ include/proxy/v4/proxy.h
@@ -1257,8 +1257,13 @@ public:
   explicit converter(F f) noexcept : f_(std::move(f)) {}
   converter(const converter&) = delete;
   template <class T>
-  operator T() && noexcept(std::is_nothrow_invocable_r_v<T, F, std::in_place_type_t<T>>) requires(std::is_invocable_r_v<T, F, std::in_place_type_t<T>>)
-  {return std::move(this->f_)(std::in_place_type<T>);}
+  operator T() && noexcept(
+      std::is_nothrow_invocable_r_v<T, F, std::in_place_type_t<T>>)
+    requires(std::is_invocable_r_v<T, F, std::in_place_type_t<T>>)
+  {
+    return std::move(this->f_)(std::in_place_type<T>);
+  }
+
 private:
   F f_;
 };
